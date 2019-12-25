956dfecce4e56bec6540ad3b42a2e2b3ae664317
Ghassan Samara <ghassan@tabtabgo.com>
2019-12-25 16:24:26 +0400

    add ts config to webpack and move index outside components folder

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -1,58 +1,61 @@
-const path = require('path');
+const path = require("path");
 
 const ROOT_PATH = path.resolve(__dirname);
 
 // Common configuration settings
 module.exports = {
   entry: {
-    'material-ui-phone-number': path.resolve(ROOT_PATH, 'src/index.js'),
+    "material-ui-phone-number": path.resolve(ROOT_PATH, "src/index.js")
   },
   resolve: {
-    extensions: ['.js', '.jsx'],
-    modules: [path.resolve(__dirname, 'node_modules')],
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+    modules: [path.resolve(__dirname, "node_modules")]
   },
   output: {
-    path: path.resolve(ROOT_PATH, 'dist'),
-    filename: 'index.js',
-    library: 'MaterialUiPhoneNumber',
+    path: path.resolve(ROOT_PATH, "dist"),
+    filename: "index.js",
+    library: "MaterialUiPhoneNumber",
     umdNamedDefine: true,
     globalObject: `(typeof self !== 'undefined' ? self : this)`, // eslint-disable-line
-    libraryTarget: 'commonjs2',
-    futureEmitAssets: true,
+    libraryTarget: "commonjs2",
+    futureEmitAssets: true
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        include: path.resolve(ROOT_PATH, 'src'),
+        include: path.resolve(ROOT_PATH, "src"),
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: [
-              '@babel/preset-env',
-              '@babel/preset-react',
-            ],
+            presets: ["@babel/preset-env", "@babel/preset-react"],
             plugins: [
-              ['@babel/plugin-proposal-class-properties', { loose: false }],
-              '@babel/plugin-proposal-object-rest-spread',
-              'lodash',
-            ],
-          },
-        },
+              ["@babel/plugin-proposal-class-properties", { loose: false }],
+              "@babel/plugin-proposal-object-rest-spread",
+              "lodash"
+            ]
+          }
+        }
       },
       {
         test: /\.png.*$/,
-        use: ['url-loader?limit=100000&mimetype=image/png'],
-        exclude: /node_modules/,
+        use: ["url-loader?limit=100000&mimetype=image/png"],
+        exclude: /node_modules/
       },
       {
         test: /\.less$/,
         use: [
-          { loader: 'style-loader' },
-          { loader: 'css-loader' },
-          { loader: 'less-loader' },
-        ],
+          { loader: "style-loader" },
+          { loader: "css-loader" },
+          { loader: "less-loader" }
+        ]
       },
-    ],
-  },
+      {
+        test: /\.(tsx|ts)?$/,
+        include: path.resolve(ROOT_PATH, "src"),
+        loader: "ts-loader",
+        exclude: /node_modules/
+      }
+    ]
+  }
 };
